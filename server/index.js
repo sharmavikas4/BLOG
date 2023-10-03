999d26abed569d082348ad02fb596d728898e44b
Vikas Sharma <103022164+sharmavikas4@users.noreply.github.com>
2023-10-03 07:53:37 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -53,7 +53,7 @@ app.use(express.static("public"));
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin',process.env.CLIENT_URL);
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type,X-Auth-Token,Origin,Authorization');
   res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
